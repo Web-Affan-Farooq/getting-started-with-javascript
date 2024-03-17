235a04170bd1ea845a0fea5c5a61bbe10e824895
web-affan farooq <affanamir903@gmail.com>
2024-03-17 18:30:38 +0500

    this is the message

--- ex-js/javascript/practice-9.js
+++ ex-js/javascript/practice-9.js
@@ -25,3 +25,33 @@ for (let city of cities) {
     console.log(city.toUpperCase());
     city++;
 }
+
+// Array methods:
+// Push():
+
+let vegitables=["potato","brinjal",'tomato', "ladyfinger"];
+
+//adding another items to list
+
+vegitables.push("chilli","okra","cucumber");
+console.log(vegitables);
+
+//pop:  It deletes the last item from list
+let deletedItems=vegitables.pop();
+console.log(vegitables);
+console.log("deleted", deletedItems);
+
+//.tostring():  It converts the whole array to number
+
+let string=vegitables.toString();
+console.log(string);
+
+//concatenetion of an array:
+console.log(heroes + vegitables);
+
+//unshift():  It add the item to the start of list
+
+vegitables.unshift("Onion");
+console.log(vegitables);
+
+//shift(): It adds an item in the start of list
